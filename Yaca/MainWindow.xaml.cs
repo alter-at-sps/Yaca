bb2c33315ccb0e23b98db20f16126bf203ef3d35
alter-at-sps <soucekd23@spstrutnov.cz>
2025-02-18 12:24:43 +0100

    hide bak display

--- Yaca/MainWindow.xaml.cs
+++ Yaca/MainWindow.xaml.cs
@@ -130,27 +130,27 @@ namespace Yaca
         {
             AccDisplay.Text = acc.ToString();
 
-            if (bak != 0)
+            /* if (bak != 0)
             {
                 BakDisplay.Text = $"BAK: {bak}";
             } else
             {
                 BakDisplay.Text = "";
-            }
+            } */
         }
 
         private void UpdateTextInputDisplay()
         {
             AccDisplay.Text = text_input;
 
-            if (bak != 0)
+            /* if (bak != 0)
             {
                 BakDisplay.Text = $"BAK: {bak}";
             }
             else
             {
                 BakDisplay.Text = "";
-            }
+            } */
         }
 
         private void EnsureAccInputIsConverted()
